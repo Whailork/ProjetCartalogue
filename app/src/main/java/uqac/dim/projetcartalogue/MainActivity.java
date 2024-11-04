64a61bb2dda2988cd136ff22cb5b5a12831c6b41
Catherine Bergeron <bergeron.catherine@icloud.com>
2024-11-04 13:43:13 -0500

    setup l'ocr avec les photos du stockage et la camera

--- app/src/main/java/uqac/dim/projetcartalogue/MainActivity.java
+++ app/src/main/java/uqac/dim/projetcartalogue/MainActivity.java
@@ -2,12 +2,17 @@ package uqac.dim.projetcartalogue;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageDecoder;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.renderscript.ScriptGroup;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -19,33 +24,68 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.odml.image.BitmapMlImageBuilder;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.TextRecognizerOptionsInterface;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+import java.io.File;
 import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnCapture, btnCopy;
+    Button btnCapture,btnCamera, btnCopy;
     TextView txtScannedData;
+    Bitmap imgBitmap;
     private static final int REQUEST_CAMERA_CODE = 100;
+    private static final int REQUEST_IMAGES_CODE = 110;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //on get les views
         btnCapture = findViewById(R.id.CaptureBtn);
+        btnCamera = findViewById(R.id.CameraBtn);
         btnCopy = findViewById(R.id.CopyTextBtn);
         txtScannedData = findViewById(R.id.scannedData);
 
+        //permission pour la camera
         if(ContextCompat.checkSelfPermission(MainActivity.this,"android.permission.CAMERA") != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{"android.permission.CAMERA"},REQUEST_CAMERA_CODE);
         }
+        //permissions pour les photos du stockage
+        if(ContextCompat.checkSelfPermission(MainActivity.this,"android.permission.READ_MEDIA_IMAGES") != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{"android.permission.READ_MEDIA_IMAGES"},REQUEST_IMAGES_CODE);
+        }
 
+        // click pour stockage
         btnCapture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent getPhoto = new Intent(Intent.ACTION_GET_CONTENT);
-                getPhoto.setType("image/*");
+                Intent getPhoto = new Intent(MediaStore.ACTION_PICK_IMAGES);
+                //getPhoto.setType("image/*");
                 startActivityForResult(getPhoto,1);
+            }
+        });
+        //click pour camera
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+
+                    Intent getPhoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(getPhoto,2);
+                }catch(Exception e){
+                    throw new RuntimeException(e);
+                }
+
             }
         });
     }
@@ -54,13 +94,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == RESULT_OK){
-            Uri imageUri = data.getData();
-            Bitmap imgBitmap = null;
-            try {
-                imgBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),imageUri);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+            if(data != null){
+                if(requestCode == 1){
+                    Uri imageUri = data.getData();
+
+                    try {
+
+                        imgBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+
+                        ExtractText(imgBitmap);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                if(requestCode == 2){
+                    try {
+
+                        Bundle extras = data.getExtras();
+                        imgBitmap = (Bitmap) extras.get("data");
+
+                        ExtractText(imgBitmap);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
+
+
         }
+    }
+
+    private void ExtractText(Bitmap bitmap){
+        TextRecognizer textRecognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        Task<Text> task = textRecognizer.process(bitmap,0);
+        task.addOnSuccessListener(new OnSuccessListener<Text>() {
+            @Override
+            public void onSuccess(Text text) {
+                txtScannedData.setText(task.getResult().getText());
+            }
+        });
+
+        //FrameLayout frame  = new FrameLayout(this);
     }
 }
