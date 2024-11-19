edb6a250474d7e8426d4f2c91881e213ab9d326b
Catherine Bergeron <bergeron.catherine@icloud.com>
2024-11-19 17:49:07 -0500

    plein de trucs avec la catalogue

--- app/src/main/java/uqac/dim/projetcartalogue/Cartalogue.java
+++ app/src/main/java/uqac/dim/projetcartalogue/Cartalogue.java
@@ -1,9 +1,11 @@
 package uqac.dim.projetcartalogue;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.JsonWriter;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.Toolbar;
@@ -16,6 +18,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Cartalogue extends AppCompatActivity {
     @Override
@@ -24,11 +27,34 @@ public class Cartalogue extends AppCompatActivity {
         setContentView(R.layout.activity_catalogue);
         RecyclerView recyclerView = findViewById(R.id.recyclerViewCarte);
         List<CarteModel> listCarte = new ArrayList<>();
-        listCarte = getData();
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
 
+            ArrayList<CarteModel> extra = getIntent().getParcelableExtra("carteList",ArrayList.class);
+            System.out.println(extra);
+            listCarte = extra;
+        }
+        //on set les images de pokemons
+        for (CarteModel carteModel:listCarte) {
+            Resources resources = getApplicationContext().getResources();
+            final int resourceId = resources.getIdentifier(carteModel.nom.toLowerCase(), "drawable", getApplicationContext().getPackageName());
+            if (resourceId == 0){
+                //on set l'image par défaut
+                carteModel.imageId = resources.getIdentifier("pokeball", "drawable", getApplicationContext().getPackageName());
+
+            }
+            else{
+                carteModel.imageId = resourceId;
+            }
+
+
+        }
+
+
+        //ne pas oublier de changer getData() pour la liste de cartes
         CardAdapter adapter = new CardAdapter(listCarte, this);
         recyclerView.setAdapter(adapter);
         GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
 
     }
@@ -38,18 +64,18 @@ public class Cartalogue extends AppCompatActivity {
     {
 
         List<CarteModel> list = new ArrayList<>();
-        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psychic"));
-        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psychic"));
-        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psychic"));
-        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psychic"));
-        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psychic"));
-        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psychic"));
-        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psychic"));
-        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psychic"));
-        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psychic"));
-        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psychic"));
-        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psychic"));
-        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psychic"));
+        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psy"));
+        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psy"));
+        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psy"));
+        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psy"));
+        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psy"));
+        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psy"));
+        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psy"));
+        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psy"));
+        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psy"));
+        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psy"));
+        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psy"));
+        list.add(new CarteModel(R.drawable.kadabra,"100", "Kadabra", "Psy"));
 
         return list;
     }
